82c53e3fb678edacdf5b8d8bad7ba9a0b9fe4e98
Brian Holderness <brian@holderness.net>
2022-11-15 07:49:08 -0700

    Initial Commit

---
+++ Presenter/PresenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresenterApp.swift
+//  Presenter
+//
+//  Created by Brian Holderness on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct PresenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
